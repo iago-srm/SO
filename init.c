fad13ffc18fe69dc7ae7ea708d025760cf3ffd9d
APF2000 <arthurpfonseca@gmail.com>
2021-05-21 00:41:49 -0300

    Arranhando o que poderia dar certo

--- init.c
+++ init.c
@@ -15,17 +15,18 @@
 
 int main()
 {
+   int last_status = -1;
+
    while (1) {
       // hello_world();
-      int last_status = -1;
       last_status = fase_1(last_status);
 
 
 	// Open file
-	char *pathname = "text.txt";
+	//char *pathname = "text.txt";
 	//int mode = ;
-	int flags = O_WRONLY; // O_CREAT
-	int status_open = open(pathname, flags);
+	//int flags = O_WRONLY; // O_CREAT
+	//int status_open = open(pathname, flags);
 
 	//printk("Status: %d", status_open);[
 	//pr_info("Teste; status: %d", status_open);
